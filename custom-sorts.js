78a71555f771f2df43eb628879e97e46cbd29f9e
danishprasla <danishprasla@hotmail.com>
2023-01-31 15:42:35 -0600

    2 done

--- custom-sorts.js
+++ custom-sorts.js
@@ -1,11 +1,23 @@
 function ageSort(users) {
   // Your code here
-  
+
   return users.sort(function (a, b) { return a.age - b.age });
 }
 
 function oddEvenSort(arr) {
   // Your code here
+  let odd = [];
+  let even = [];
+
+  for (let num of arr) {
+    if (num % 2 === 0) even.push(num);
+    else odd.push(num);
+  }
+  let evenSorted = even.sort(function (a, b) { return a - b })
+  let oddSorted = odd.sort(function (a, b) { return a - b })
+  // console.log(evenSorted);
+  // console.log(oddSorted)
+  return [...oddSorted, ...evenSorted]
 }
 
 function validAnagrams(s, t) {
